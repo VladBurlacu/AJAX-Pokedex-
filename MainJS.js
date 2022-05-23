c10e710051ab93588957955cbfc0bcaa1958352c
Vlad Burlacu <vladburlacu1337@gmail.com>
2022-05-23 14:57:15 +0200

    added function to turn the input text into lower case, fetched moves and weight from the api

--- MainJS.js
+++ MainJS.js
@@ -1,15 +1,20 @@
 document.querySelector(`#searchBtn`).addEventListener(`click`, getPokemon);
 
+function lowerCaseName(string) {
+    return string.toLowerCase();
+}
+
 function getPokemon(e) {
     const name = document.querySelector(`#searchQuery`).value;
+    const pokemonName = lowerCaseName(name);
 
-    fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
+    fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
         .then((response) => response.json())
         .then((data) =>{
             document.querySelector(".pokemonCard").innerHTML = `
             <div>
                 <img 
-                src="${data.sprites.other["official-artwork"].front_default}
+                src="${data.sprites.other["official-artwork"].front_default}"
                 alt="${data.name}"
                 />
             </div>
