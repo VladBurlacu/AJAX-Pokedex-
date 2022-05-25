ffae8aacde09ba25039d79780170f62072af0769
Vlad Burlacu <vladburlacu1337@gmail.com>
2022-05-25 16:15:59 +0200

    small changes

--- MainJS.js
+++ MainJS.js
@@ -24,13 +24,16 @@ document.querySelector(`#searchBtn`).addEventListener(`click`, async function ge
     const evoFetchChain = await fetch(evoFetchChainUrl);
     let evoChainData = await evoFetchChain.json();
 
+    let baseForm = evoChainData.chain.species.name;
     let evoName = evoChainData.chain.evolves_to[0].species.name;
     let evoName2 = evoChainData.chain.evolves_to[0].evolves_to[0].species.name;
-    const evoFetch = await fetch(`https://pokeapi.co/api/v2/pokemon/${evoName}`);
-    let evoNameData = await evoFetch.json();
-    let baseForm = evoChainData.chain.species.name;
+
     const baseFormFetch = await fetch(`https://pokeapi.co/api/v2/pokemon/${baseForm}`)
     let baseFormData = await baseFormFetch.json();
+
+    const evoFetch = await fetch(`https://pokeapi.co/api/v2/pokemon/${evoName}`);
+    let evoNameData = await evoFetch.json();
+
     const secondEvoFetch = await fetch(`https://pokeapi.co/api/v2/pokemon/${evoName2}`)
     let secondEvoData = await secondEvoFetch.json();
 
@@ -40,23 +43,29 @@ document.querySelector(`#searchBtn`).addEventListener(`click`, async function ge
                
                 <div class="pokemonEvo">
                 
-                    <div>Base Form: ${baseForm}
+                    <div>
+                    Base Form: ${baseForm}
                     <img 
                     src="${baseFormData.sprites.other["official-artwork"].front_default}"
                     alt=""
-                    /></div>
+                    />
+                    </div>
                 
-                    <div>Second Evolution: ${evoName}
+                    <div>
+                    Second Evolution: ${evoName}
                     <img 
                     src="${evoNameData.sprites.other["official-artwork"].front_default}"
                     alt=""
-                    /></div>
+                    />
+                    </div>
                 
-                    <div>Third Evolution: ${evoName2}
+                    <div>
+                    Third Evolution: ${evoName2}
                     <img 
                     src="${secondEvoData.sprites.other["official-artwork"].front_default}"
                     alt=""
-                    /></div>
+                    />
+                    </div>
                 
                 </div>
                 `;
